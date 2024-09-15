56f54d94db7266ae4f5cadfeff0975fc639acf97
Andrew Dunn <atd2005@gmail.com>
2024-09-14 20:51:48 -0500

    Initial commit

---
+++ test/fargate_pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FargatePipeline from '../lib/fargate_pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fargate_pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FargatePipeline.FargatePipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
