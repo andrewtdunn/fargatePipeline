66b17322d4aad0422ae5413f4655f8cfd6b75c02
Andrew Dunn <atd2005@gmail.com>
2024-09-15 00:50:39 -0500

    add prebuild

--- lib/fargate_pipeline-stack.ts
+++ lib/fargate_pipeline-stack.ts
@@ -1,7 +1,13 @@
 import * as cdk from "aws-cdk-lib";
-import { CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { FargatePipelineStage } from "./fargate_pipeline-stage";
+import { Repository } from "aws-cdk-lib/aws-ecr";
+import { Role } from "aws-cdk-lib/aws-iam";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 const GITHUB_CONNECTION_ARN: string =
@@ -11,6 +17,23 @@ export class FargatePipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const ECR_REPO_ARN =
+      "arn:aws:ecr:us-east-1:637423577773:repository/demo_repo";
+
+    const ecrRepo = Repository.fromRepositoryArn(this, "ecrRepo", ECR_REPO_ARN);
+
+    const REPOSITORY_URI = ecrRepo.repositoryUri;
+
+    const preBuildRole = new Role(this, "preBuildRole", {
+      assumedBy: new cdk.aws_iam.ServicePrincipal("codebuild.amazonaws.com"),
+    });
+
+    preBuildRole.addManagedPolicy(
+      cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "AmazonEC2ContainerRegistryFullAccess"
+      )
+    );
+
     const codeRepo = CodePipelineSource.connection(
       "andrewtdunn/fargatePipeline",
       "main",
@@ -19,12 +42,49 @@ export class FargatePipelineStack extends cdk.Stack {
       }
     );
 
+    const COMMIT_ID = codeRepo.sourceAttribute("CommitId");
+
+    const preBuildStep = new CodeBuildStep("PreBuildStep", {
+      input: codeRepo,
+      commands: [
+        'timeout 15 sh -c "until docker info; do echo .; sleep 1; done"',
+        "aws --version",
+        'export DOCKER_DEFAULT_PLATFORM="linux/amd64"',
+        `aws ecr get-login-password --region ${process.env.region} | docker login --username AWS --password-stdin ${REPOSITORY_URI}`,
+        `COMMIT_HASH=${COMMIT_ID}`,
+        "IMAGE_TAG=${COMMIT_HASH:=latest}",
+        "echo Build started on `date`",
+        "echo Building the Docker image...",
+        `export REPOSITORY_URI='${REPOSITORY_URI}'`,
+        "pwd",
+        "cd azure_query_async",
+        //"docker build -f ./Dockerfile.codebuild -t $REPOSITORY_URI:latest .",
+        `docker build -t demo_repo --platform linux/amd64 .`,
+        "docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG",
+        "cd ..",
+        "echo Build completed on `date`",
+        "echo Pushing the Docker images...",
+        "docker push $REPOSITORY_URI:latest",
+        "docker push $REPOSITORY_URI:$IMAGE_TAG",
+        "echo Writing image definitions file...",
+        `printf \'[{"name":"%s","imageUri":"%s"}]\' ${ecrRepo.repositoryName} $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json`,
+      ],
+      primaryOutputDirectory: "./",
+      env: {
+        COMMIT_ID,
+      },
+      role: preBuildRole,
+    });
+
     const pipeline = new CodePipeline(this, "FargatePipeline", {
       pipelineName: "FargatePipeline",
       crossAccountKeys: true,
       synth: new cdk.pipelines.ShellStep("SynthStep", {
-        input: codeRepo,
+        input: preBuildStep,
         commands: ["npm ci", "npm run build", "npx cdk synth"],
+        env: {
+          COMMIT_ID,
+        },
       }),
     });
 
